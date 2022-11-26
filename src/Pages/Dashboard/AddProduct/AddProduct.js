32a076453ea5e724369cbda4e0f4051c440b5f9c
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-11-26 17:04:35 +0600

    add product layout added

--- src/Pages/Dashboard/AddProduct/AddProduct.js
+++ src/Pages/Dashboard/AddProduct/AddProduct.js
@@ -22,7 +22,7 @@ const AddProduct = () => {
 
   const handleAddProduct = (data) => {
     console.log(data);
-    const id = categories.find(cId => data.role === cId.category_name);
+    const id = categories.find(cId => data.category_name === cId.category_name);
     console.log(id._id)
   }
 
@@ -32,7 +32,7 @@ const AddProduct = () => {
       <div className='m-5'>
         <form onSubmit={handleSubmit(handleAddProduct)}>
 
-          <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-5 mb-5'>
             <div className="form-control">
               <label className="label"><span className="label-text">Product Name</span> </label>
               <input {...register("name")} type="name" placeholder="Product Name" className="input input-bordered " />
@@ -41,25 +41,80 @@ const AddProduct = () => {
 
             <div className="form-control">
               <label className="label"><span className="label-text">Photo</span></label>
-              <input {...register("file")} type="file" className="file-input file-input-bordered" />
+              <input {...register("photo")} type="file" className="file-input file-input-bordered" />
               {/* {errors.file && <p className="text-red-500"><small>*{errors?.file?.message}</small></p>} */}
             </div>
 
             <div className="form-control">
-            <label className="label"><span className="label-text">Select Category</span> </label>
-            <select {...register("role")} className="select select-bordered">
-              {
-                categories.map(category => <option key={category._id}>{category.category_name}</option>)
-              }
-            </select>
-            {errors.role && <p className="text-red-500"><small>*{errors?.role?.message}</small></p>}
+              <label className="label"><span className="label-text">Select Category</span> </label>
+              <select {...register("category_name")} className="select select-bordered">
+                {
+                  categories.map(category => <option key={category._id}>{category.category_name}</option>)
+                }
+              </select>
+              {/* {errors.role && <p className="text-red-500"><small>*{errors?.role?.message}</small></p>} */}
+            </div>
           </div>
+
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-5 mb-5'>
+            <div className="form-control">
+              <label className="label"><span className="label-text">Contact Number</span> </label>
+              <input {...register("phone_number")} type="number" placeholder="Contact Number" className="input input-bordered " />
+              {/* {errors.name && <p className="text-red-500"><small>*{errors?.name?.message}</small></p>} */}
+            </div>
+
+            <div className="form-control">
+              <label className="label"><span className="label-text">Location</span> </label>
+              <input {...register("location")} type="text" placeholder="Location" className="input input-bordered " />
+              {/* {errors.name && <p className="text-red-500"><small>*{errors?.name?.message}</small></p>} */}
+            </div>
+
+            <div className="form-control">
+              <label className="label"><span className="label-text">Condition</span> </label>
+              <select {...register("condition")} className="select select-bordered">
+                <option>Excellent</option>
+                <option>Good</option>
+                <option>Average</option>
+                <option>Low</option>
+              </select>
+              {/* {errors.role && <p className="text-red-500"><small>*{errors?.role?.message}</small></p>} */}
+            </div>
           </div>
 
+          <div className='grid grid-cols-1 md:grid-cols-4 gap-5 mb-5'>
+            <div className="form-control">
+              <label className="label"><span className="label-text">Purchase Year</span> </label>
+              <input {...register("year_of_purchase")} type="text" placeholder="Purchase Year" className="input input-bordered " />
+              {/* {errors.name && <p className="text-red-500"><small>*{errors?.name?.message}</small></p>} */}
+            </div>
 
-          
+            <div className="form-control">
+              <label className="label"><span className="label-text">Product Used Duration</span> </label>
+              <input {...register("year_of_use")} type="text" placeholder="Used Duration" className="input input-bordered " />
+              {/* {errors.name && <p className="text-red-500"><small>*{errors?.name?.message}</small></p>} */}
+            </div>
 
-          <input className='btn btn-primary w-full my-5 max-w-xs' type="submit" value='Add A Product' />
+            <div className="form-control">
+              <label className="label"><span className="label-text">Original Price</span> </label>
+              <input {...register("original_price")} type="text" placeholder="Original Price" className="input input-bordered " />
+              {/* {errors.name && <p className="text-red-500"><small>*{errors?.name?.message}</small></p>} */}
+            </div>
+
+            <div className="form-control">
+              <label className="label"><span className="label-text">Resell Price</span> </label>
+              <input {...register("resell_price")} type="text" placeholder="Resell Price" className="input input-bordered " />
+              {/* {errors.name && <p className="text-red-500"><small>*{errors?.name?.message}</small></p>} */}
+            </div>
+
+          </div>
+
+          <div className="form-control">
+            <label className="label"><span className="label-text">Description</span> </label>
+            <textarea {...register("description")} className="textarea textarea-bordered" placeholder="Bio"></textarea>
+            {/* {errors.name && <p className="text-red-500"><small>*{errors?.name?.message}</small></p>} */}
+          </div>
+
+          <input className='btn btn-primary w-full my-5' type="submit" value='Add A Product' />
         </form>
       </div>
     </div>
