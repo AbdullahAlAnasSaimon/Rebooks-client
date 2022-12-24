652d30eb005205dbf8d5b05b77fbae3b50e1dc28
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-12-25 00:20:45 +0600

    user profile page re-designed

--- src/Pages/Dashboard/MyProfile/MyProfile.js
+++ src/Pages/Dashboard/MyProfile/MyProfile.js
@@ -11,9 +11,9 @@ const MyProfile = () => {
   useTitle('Dashboard');
   const {user} = useContext(AuthContext);
 
-  const [isAdminLoading] = useAdmin(user?.email);
-  const [isSellerLoading] = useSeller(user?.email);
-  const [isBuyerLoading] = useBuyer(user?.email);
+  const [isAdmin, isAdminLoading] = useAdmin(user?.email);
+  const [isSeller, isSellerLoading] = useSeller(user?.email);
+  const [isBuyer, isBuyerLoading] = useBuyer(user?.email);
 
   if(isAdminLoading || isSellerLoading || isBuyerLoading){
     return <Loading/>
@@ -22,15 +22,17 @@ const MyProfile = () => {
   return (
     <div>
       <h2 className='text-2xl my-5 text-center md:text-left font-bold'>My profile</h2>
-      <div className='w-11/12 mx-auto mt-10'>
+      <div className='w-11/12 mx-auto mt-10 flex flex-col md:flex-row justify-around'>
         <div className="avatar block">
-          <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 mx-auto md:mx-0">
-          {user?.photoURL ? <img src={user?.photoURL} alt='' /> : <FaUserCircle className='text-[96px] text-gray-700' />}
+          <div className="w-24 md:w-48 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 mx-auto md:mx-0">
+          {user?.photoURL ? <img src={user?.photoURL} alt='' /> : <FaUserCircle className='text-[96px] md:text-[192px] text-gray-700' />}
           </div>
         </div>
         <div className='mt-10 w-8/12 md:w-auto mx-auto md:mx-0'>
           <label className='label' htmlFor="">Name</label>
         <input type="text" className="input input-bordered w-full max-w-xs" defaultValue={user?.displayName} readOnly/>
+          <label className='label' htmlFor="">Account Type</label>
+        <input type="text" className="input input-bordered w-full max-w-xs" defaultValue={(isAdmin && "Admin") || (isSeller && "Seller") || (isBuyer && "Buyer")} readOnly/>
           <label className='label' htmlFor="">Email</label>
         <input type="text" className="input input-bordered w-full max-w-xs" defaultValue={user?.email} readOnly/>
         </div>
