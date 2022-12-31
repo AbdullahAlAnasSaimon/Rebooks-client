c542869438a3573742d200b27b6e58d637eb045d
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-12-31 23:50:28 +0600

    modified button loading size

--- src/Pages/Shared/Loading/ButtonLoading.js
+++ src/Pages/Shared/Loading/ButtonLoading.js
@@ -3,8 +3,8 @@ import React from 'react';
 const ButtonLoading = () => {
   return (
     <svg className="animate-spin h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-      <circle className="opacity-100" cx="12" cy="12" r="10" stroke="#3c3d3e" stroke-width="4"></circle>
-      <path className="opacity-75" fill="#000000" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+      <circle className="opacity-75" cx="12" cy="12" r="10" stroke="#dddddd" stroke-width="4"></circle>
+      <path className="opacity-75" fill="#ffffff" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
     </svg>
   );
 };
