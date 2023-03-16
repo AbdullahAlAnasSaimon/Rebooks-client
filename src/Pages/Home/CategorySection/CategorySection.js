03bb0c7ecc88bc3e82669aca90c06405eae4135d
abdullah <abdullahalanassaimon.swd@gmail.com>
2023-03-16 23:23:27 +0600

    changed the layout of button

--- src/Pages/Home/CategorySection/CategorySection.js
+++ src/Pages/Home/CategorySection/CategorySection.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 const CategorySection = ({ category }) => {
   return (
-    <Link to={`/category/${category?._id}`}><div className='bg-blue-100 hover:bg-blue-200 duration-300 py-3 border-2 border-blue-300 rounded-lg'>
-      <p>{category?.category_name}</p>
+    <Link to={`/category/${category?._id}`}><div className='bg-blue-100 hover:bg-blue-200 duration-300 py-3 border-2 border-blue-200 rounded-full'>
+      <p className='text-lg font-semibold'>{category?.category_name}</p>
     </div></Link>
   );
 };
